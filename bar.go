2eb4618f7d0d934bd22572b8722343263de6405b
vermaharshh <67313629+vermaharshh@users.noreply.github.com>
2021-03-21 22:27:58 +0530

    Add files via upload

---
+++ bar.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"time"
+
+	"github.com/cheggaaa/pb/v3"
+)
+
+func main() {
+	count := 100000
+	// create and start new bar
+	bar := pb.StartNew(count)
+
+	for i := 0; i < count; i++ {
+		bar.Increment()
+		time.Sleep(time.Millisecond)
+	}
+	bar.Finish()
+}
